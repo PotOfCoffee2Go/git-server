b90a83f35e3abb591ed90e6befdd9d71f3cb4109
PotOfCoffee2Go <PotOfCoffee2Go@users.noreply.github.com>
2025-08-03 16:32:26 -0400

    refactor: index.js - prepare for moving code into modules './lib'
    - bump to v1.2.1
    - add comments to .gitignore

--- index.js
+++ index.js
@@ -3,22 +3,26 @@ const os = require('node:os');
 const path = require('node:path');
 
 // --------------------------------
-// Configuration
+// Start of Configuration
 
-// Recommend leaving as 'http'
-//  - will be localhost so access only allowed from machine running the server
-const protocol = 'http'; // or https
-const port = 7005;
-
-// Directory that contains the repositories
+// Directory that contains the repos
+// Automatically created if not present
 const repoDir = './repos';
 
-// Proxy server front-ends the repo server - network access is thru the proxy
-const allowNetworkAccess = true; // false = do not start proxy server
-const proxyhost = '0.0.0.0';
+// Repo server
+const port = 7005;
+
+// Proxy server - network access is thru the proxy
 const proxyport = 7000;
 
-// users.json - if exists else use demo users
+// allowNetworkAccess equals true = the proxy server will be started
+const allowNetworkAccess = true; // false = do not start proxy server
+
+// allowAnonymousPush equals true = user/password checking will NOT BE PERFORMED!
+const allowAnonymousPush = true; // false - require user/password to 'push'
+
+// If allowAnonymousPush equals false - check user/passwords
+// Use 'users.json' file if exists else use default users listed below
 // Format of data in the JSON file is array of user/passwords as shown below
 var users = fs.existsSync('./users.json') ? require('./users.json') : false;
 if (!users) {
@@ -28,6 +32,9 @@ if (!users) {
 		{ "name": "42", "password": "42" }
 	];
 }
+
+// End of configuration
+// --------------------------------
 
 // --------------------------------
 // Helpers
@@ -40,9 +47,11 @@ const auth = (users, inName, inPw) => {
 // Authenticate a user
 const authenticate = ({ type, repo, user, headers }, next) => {
 	console.log(type, repo, headers);
-	if (type == 'push') { // only authenticated users allowed to push
+	if (type == 'push') {
+		// anonymous push allowed?
+		if (!users || allowAnonymousPush) { next(); return; }
 		user((username, password) => {
-			if (users && auth(users, username, password)) {
+			if (auth(users, username, password)) {
 				next();
 			} else {
 				next(hue('wrong password',9));
@@ -53,14 +62,21 @@ const authenticate = ({ type, repo, user, headers }, next) => {
 	}
 }
 
-// ---------
+console.log(`Anonymous 'push' is ${allowAnonymousPush ? 'allowed' : 'not allowed'}`);
+console.log(`Access from other devices on network is ${allowNetworkAccess ? 'allowed' : 'not allowed'}`);
+console.log('');
+
+// --------------------------------
 // Servers
+
 // Proxy server allows access by network devices
-if (allowNetworkAccess) {
-	const httpProxy = require('http-proxy');
-	httpProxy.createProxyServer({ target:`http://localhost:${port}` })
-		.listen({host: proxyhost, port: proxyport});
-	console.log(hue(`Proxy server on http://${os.hostname()}:${proxyport} -> http://localhost:${port}`));
+const proxyServer = () => {
+	if (allowNetworkAccess) {
+		const httpProxy = require('http-proxy');
+		httpProxy.createProxyServer({ target:`http://localhost:${port}` })
+			.listen({host: '0.0.0.0', port: proxyport});
+		console.log(hue(`Proxy server running at http://${os.hostname()}:${proxyport} -> http://localhost:${port}`));
+	}
 }
 
 // Repo server
@@ -68,23 +84,25 @@ const repoPath = path.normalize(path.resolve(repoDir));
 const { Git: Server } = require('node-git-server');
 const repos = new Server(repoPath, { authenticate, autoCreate: true });
 
-repos.listen( port, { type: protocol }, (error) => {
+repos.listen( port, { type: 'http' }, (error) => {
 	if (error) {
 		return console.error(hue(`failed to start git-server because of error ${error}`));
 	}
-	console.log(hue(`git-server running at ${protocol}://localhost:${port}`));
 	console.log(hue(`Repositories in directory: ${repoPath}`));
+	console.log(hue(`git-server running at http://localhost:${port}`));
+	proxyServer();
 	repos.list((err, result) => {
 		if (!result) {
 			console.log('No repositories available...');
 		} else {
 			console.log(result);
 		}
+		console.log(hue('(press ctrl-C to exit)',9));
 	});
 });
 
-// ---------
-// git command / event listeners
+// --------------------------------
+// Examples of git command event listeners
 repos.on('push', (push) => {
 	console.log(`push ${push.repo} / ${push.commit} ( ${push.branch} )`);
 	repos.list((err, results) => {
