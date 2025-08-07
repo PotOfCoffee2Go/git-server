e2fe30cd609e46fd690e2acff3118a18759f063c
PotOfCoffee2Go <PotOfCoffee2Go@users.noreply.github.com>
2025-08-07 12:22:05 -0400

    refactor: add REPL as debugging tool
    - bump to v1.4.0
    - add replprompt.js implements node:repl
    - add ref to proxy and repos server instances to config object
    - pass config object to modules instead of specific vars
    - moved config layout version check to index.js
    - add .dir command to repl
    - display on startup to type .help for list of commands

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ const pkg = require('./package.json');
 
 const { configPath } = require('./lib/parameters').parameters();
 const config = require(configPath).config();
+
+if (config.layout !== 'v1') {
+	console.log(hue(`Unknown configuration layout: "${layout}"`,9));
+	console.log('Must be layout "v1" for git-server v${pkg.version}');
+	process.exit(1);
+}
+
 config.repoDir = path.parse(configPath).name;
+config.server = {};
  
 const { repoServer } = require('./lib/reposerver');
 
