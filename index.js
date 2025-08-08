80431277adebcb03a74074c204b86c3d0f5cd945
PotOfCoffee2Go <PotOfCoffee2Go@users.noreply.github.com>
2025-08-08 08:58:05 -0400

    refactor: reorganize index.js code lines
    - bump to v1.4.2
    - rebuild docs/index.html

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const path = require('node:path');
-
+const pkg = require('./package.json');
 const hue = (txt, nbr=214) => `\x1b[38;5;${nbr}m${txt}\x1b[0m`;
 
-const pkg = require('./package.json');
-
+const { repoServer } = require('./lib/reposerver');
 const { configPath } = require('./lib/parameters').parameters();
+
 const config = require(configPath).config();
+config.repoDir = path.parse(configPath).name;
+config.server = {};
 
 if (config.layout !== 'v1') {
 	console.log(hue(`Unknown configuration layout: "${layout}"`,9));
@@ -13,13 +15,8 @@ if (config.layout !== 'v1') {
 	process.exit(1);
 }
 
-config.repoDir = path.parse(configPath).name;
-config.server = {};
- 
-const { repoServer } = require('./lib/reposerver');
-
-process.chdir(path.dirname(configPath));
-
 console.log(`git-server v${pkg.version}`);
 console.log(hue(`Configuration file: ${configPath}\n`));
+
+process.chdir(path.dirname(configPath));
 repoServer(config);
